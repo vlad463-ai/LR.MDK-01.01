4a0e74a65abf3ae5f228592668ab909f25c025fe
Vladimir420911 <dhbtgvhb@gmail.com>
2025-09-20 12:59:55 +0300

    Сделали программу которая проверяет есть ли в ведденном пользователем слове одинаковые буквы с нашим списком(text) и если есть то выводит полное слово в другой список

--- LIST Prakt/LIST Prakt/Program.cs
+++ LIST Prakt/LIST Prakt/Program.cs
@@ -11,20 +11,46 @@ namespace LIST_Prakt
         {
             ///создали лист-список
             List<string> text = new List<string>();
+            
             ///Заполнили список пятью элементами
             text.Add("grup");
             text.Add("PC");
             text.Add("film");
             text.Add("case");
             text.Add("box");
+            
             /// Ввод текста с клавиатуры
             Console.WriteLine("Введите текст: ");
             string user = Console.ReadLine();
+            //Console.WriteLine(user);
+
+            List<string> input = new List<string>();
+            
+            for(int i =0; i< text.Count;i++)
+            {
+                if (text[i].Contains(user))
+                {
+                    input.Add(text[i]);
+                }
+            }
+            foreach(string m in input)
+            {
+                Console.WriteLine(m);
+            }
+            
+            
+            
+
+
+
+
 
 
 
 
 
         }
+
+        
     }
 }
