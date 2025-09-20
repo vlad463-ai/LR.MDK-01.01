78f256322708b5c3f199e4613c7900d8c998c0c5
Vladimir420911 <dhbtgvhb@gmail.com>
2025-09-20 11:54:45 +0300

    Создали список с 5 элементами

---
+++ LIST Prakt/LIST Prakt/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections.Generic;
+
+
+namespace LIST_Prakt
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> text = new List<string>();
+
+            text.Add("grup");
+            text.Add("PC");
+            text.Add("film");
+            text.Add("case");
+            text.Add("box");
+
+
+
+
+
+        }
+    }
+}
---
+++ LIST Prakt/LIST Prakt/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("LIST Prakt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LIST Prakt")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("85a71463-b426-4bf2-9140-d144c3610b1f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
