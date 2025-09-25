6119c84745baafc9012ca0ffe02f8cff9dc77877
YanaTyzik <denisyuk_yanina@mail.ru>
2025-09-25 08:23:15 +0300

    Создал массив, размер которого пользователь вводит сам и вывел массив на консоль

---
+++ LR 1/rabota №1/rabota №1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rabota__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // С клавиатуры ввести массив из n целых чисел.Подсчитать количество четных и нечетных элементов.
+            Console.WriteLine("Введите размер массива");
+            int razMas = Convert.ToInt32(Console.ReadLine());
+            int [] array = new int[razMas]; 
+            
+            foreach(int element in array)
+            {
+                Console.WriteLine(element);
+            }
+            
+
+        }
+    }
+}
