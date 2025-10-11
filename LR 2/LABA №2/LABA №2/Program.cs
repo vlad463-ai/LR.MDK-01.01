ea0500caca4652cbe87f057b260924d15cae46b8
sellasouls666 <pgbc303@gmail.com>
2025-10-11 11:41:33 +0300

    Создал проект в папке LR 2

---
+++ LR 2/LABA №2/LABA №2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABA__2
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
