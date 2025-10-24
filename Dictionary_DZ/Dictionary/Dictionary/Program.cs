4f042bf2d43d21d2057332c5a39f35d42c6ba7c9
nikand32543 <vd4216485@gmail.com>
2025-10-24 21:23:34 +0300

    Взял код для ДЗ

--- Dictionary_DZ/Dictionary/Dictionary/Program.cs
+++ Dictionary_DZ/Dictionary/Dictionary/Program.cs
@@ -6,10 +6,103 @@ using System.Threading.Tasks;
 
 namespace Dictionary
 {
+    enum Genre
+    {
+        Drama,
+        Tragic,
+        Comedy,
+        Unknown
+    }
     internal class Program
     {
+        static public void RemoveBook(Dictionary<Genre, List<string>> allBooks,
+                                   string bookName)
+        {
+            foreach (List<string> books in allBooks.Values)
+            {
+                for (int index = 0; index < books.Count; ++index)
+                {
+                    if (books[index] == bookName)
+                    {
+                        books.RemoveAt(index);
+                        --index;
+                    }
+                }
+            }
+        }
+
+        static void PrintBooksByGenre(Genre genre, Dictionary<Genre, List<string>> allBooks)
+        {
+            if (allBooks.ContainsKey(genre))
+            {
+                List<string> books = allBooks[genre];
+                Console.WriteLine(String.Join("\n", books));
+            }
+            else
+            {
+                Console.WriteLine("Книг данного жанра мы еще не завезли");
+            }
+        }
+
+        static private Genre MakeGenreByText(string text)
+        {
+            if (text == "драма")
+            {
+                return Genre.Drama;
+            }
+
+            if (text == "комедия")
+            {
+                return Genre.Comedy;
+            }
+
+            if (text == "трагедия")
+            {
+                return Genre.Tragic;
+            }
+
+            return Genre.Unknown;
+        }
+        
         static void Main(string[] args)
         {
+            /// создали пустой словарь,
+            /// где ключом выступает тип перечисления Genre,
+            /// а значение список строковых значений (список названий книг)
+            Dictionary<Genre, List<string>> catalogBooks =
+                new Dictionary<Genre, List<string>>();
+
+            /// заполнили книги жанра комедии
+            catalogBooks.Add(Genre.Comedy,
+                             new List<string>() { "Горе от ума",
+                                                  "Вечера на хуторе близ Диканьки",
+                                                  "Ревизор",
+                                                  "Ум от горя"});
+
+            /// заполнили книги жанра трагедии
+            catalogBooks.Add(Genre.Tragic,
+                 new List<string>() { "Сегодня суббота",
+                                      "Трагедия понедельника"});
+
+            Console.Write("Какой жанр литературы Вам интересен? ");
+            string text = Console.ReadLine();
+            Genre genre = MakeGenreByText(text);
+
+            if (genre == Genre.Unknown)
+            {
+                Console.WriteLine("Ай я-яй, нужна либо драма, либо трагедия, либо комедия");
+                return;
+            }
+
+            PrintBooksByGenre(genre, catalogBooks);
+
+            Console.Write("Какую книгу хотите удалить?");
+            Console.WriteLine();
+            string bookRemove = Console.ReadLine();
+            RemoveBook(catalogBooks, bookRemove);
+            Console.WriteLine("Набор книг после удаления");
+            Console.WriteLine();
+            PrintBooksByGenre(genre, catalogBooks);
         }
     }
 }
