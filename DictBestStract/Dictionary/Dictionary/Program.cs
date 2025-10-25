84dd1e9c016abb4b23b216f1c5d058e0346790fc
ziqara <tcvegor2006@mail.ru>
2025-10-25 11:52:01 +0300

    Создали ветвь в которой создали пустой словарь

---
+++ DictBestStract/Dictionary/Dictionary/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> Dictionary = new Dictionary<string, List<string>>();
+        }
+    }
+}
