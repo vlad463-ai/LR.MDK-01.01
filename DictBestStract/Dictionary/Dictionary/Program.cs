b10a74e8dea8f048d72b55cc31f04a7b106dff93
ziqara <tcvegor2006@mail.ru>
2025-10-25 13:02:30 +0300

    Реализовал функцию по выводу числа марок авто и всех марок авто(проверки сделать не успели):

--- DictBestStract/Dictionary/Dictionary/Program.cs
+++ DictBestStract/Dictionary/Dictionary/Program.cs
@@ -2,11 +2,21 @@
 
 using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace Dictionary
 {
     class Program
     {
+        static public void AllElementValues(Dictionary<string, List<string>> Dictionary)
+        {
+            Console.WriteLine("Какая цена вам интересна (Дорогие, Средние, Дешёвые): ");
+            string UserRequest = Console.ReadLine();
+            List<string> brands = Dictionary[UserRequest];
+            Console.WriteLine("Число марок автомобилей: " +Dictionary.Values.Count);
+            Console.Write("Все марки автомобилей: " +string.Join(", ", brands));
+            
+        }
         static void Main(string[] args)
         {
             //Создание словаря
@@ -17,7 +27,14 @@ namespace Dictionary
             Dictionary.Add("Дешёвые", new List<string>() { "Двенашка", "Семёрка", "Четырка" });
             // Вывод на экран всех ключей
             Console.Write(string.Join(", ", Dictionary.Keys));
-
+            // Вывод на экран значения через запятую по любому ключу
+            Console.WriteLine();
+            Console.WriteLine("Какой тип машины вам интересен: ");
+            string a = Console.ReadLine();
+            List<string> price = Dictionary[a];
+            Console.WriteLine(string.Join(",", price));
+            //Зпрос ключа пользователем по запросу выводит число элементов значения указанного ключа
+            AllElementValues(Dictionary);
 
 
 
