5e1e83fa393d117ea0e1dbbe5015eb4cfe57963d
ziqara <tcvegor2006@mail.ru>
2025-10-25 12:02:55 +0300

    Добавили в словарь 3 ключи и заполнили для ключей их значения

--- DictBestStract/Dictionary/Dictionary/Program.cs
+++ DictBestStract/Dictionary/Dictionary/Program.cs
@@ -9,6 +9,9 @@ namespace Dictionary
         static void Main(string[] args)
         {
             Dictionary<string, List<string>> Dictionary = new Dictionary<string, List<string>>();
+            Dictionary.Add("дорогие", new List<string>() {"Бентли","Ламборгини","Кадиллак"});
+            Dictionary.Add("средние", new List<string>() { "Форд", "Ниссан", "Шевроле" });
+            Dictionary.Add("дешовые", new List<string>() { "Двинка", "Семёрка", "Четырка" });
         }
     }
 }
