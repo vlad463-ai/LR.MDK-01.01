a3238ba9e42f9d7cc0a6fc5e0f8d17e773543ec0
ziqara <tcvegor2006@mail.ru>
2025-10-25 12:24:57 +0300

    Произвели вывод на консоль всех ключей из словаря

--- DictBestStract/Dictionary/Dictionary/Program.cs
+++ DictBestStract/Dictionary/Dictionary/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using System;
 using System.Collections.Generic;
 
 namespace Dictionary
@@ -8,10 +9,19 @@ namespace Dictionary
     {
         static void Main(string[] args)
         {
-            Dictionary<string, List<string>> Dictionary = new Dictionary<string, List<string>>();
-            Dictionary.Add("дорогие", new List<string>() {"Бентли","Ламборгини","Кадиллак"});
-            Dictionary.Add("средние", new List<string>() { "Форд", "Ниссан", "Шевроле" });
-            Dictionary.Add("дешовые", new List<string>() { "Двинка", "Семёрка", "Четырка" });
+            //Создание словаря
+            Dictionary<string, List<string>> Dictionary = new Dictionary<string, List<string>>(); 
+            //Заполнение словаря
+            Dictionary.Add("Дорогие", new List<string>() {"Бентли"}); 
+            Dictionary.Add("Средние", new List<string>() { "Форд", "Ниссан" });
+            Dictionary.Add("Дешёвые", new List<string>() { "Двенашка", "Семёрка", "Четырка" });
+            // Вывод на экран всех ключей
+            Console.Write(string.Join(", ", Dictionary.Keys));
+
+
+
+
+
         }
     }
 }
