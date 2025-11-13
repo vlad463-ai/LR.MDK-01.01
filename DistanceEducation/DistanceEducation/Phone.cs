1f3d91e0eb9178c7be6420b1214e444fc50f023a
ziqara <tcvegor2006@mail.ru>
2025-11-13 10:39:25 +0300

    В структуре Phone реализовал функцию для вычесления общей суммы проданного за период

--- DistanceEducation/DistanceEducation/Phone.cs
+++ DistanceEducation/DistanceEducation/Phone.cs
@@ -10,5 +10,28 @@ namespace DistanceEducation
     {
         public string Model;
         public int Price;
+
+        static public void CalculateTotalSales(List<SalePhone> Sale_phones, List<Phone> Phones) //вычесляем общую сумму проданного за определенный период
+        {
+            DateTime start = new DateTime(2024, 11, 1);
+            DateTime end = new DateTime(2024, 11, 5);
+
+            int AmountSales = 0;
+            foreach (SalePhone sale in Sale_phones)
+            {
+                if (sale.Date >= start && sale.Date <= end)
+                {
+                    foreach (Phone Phone in Phones)
+                    {
+                        if (Phone.Model == sale.PhoneModel)
+                        {
+                            AmountSales += Phone.Price * sale.Sold;
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Общая сумма продаж" + AmountSales);
+        }
     }
 }
