019e4ad95ffed0ef650615bd32e5c4068b160a5b
ziqara <tcvegor2006@mail.ru>
2025-11-13 10:37:15 +0300

    Добавил структуру SalePhone которая имеет (Sold, PhoneModel, Date)

---
+++ DistanceEducation/DistanceEducation/SalePhone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceEducation
+{
+    public struct SalePhone
+    {
+        public int Sold;
+        public string PhoneModel;
+        public DateTime Date;
+
+    }
+}
