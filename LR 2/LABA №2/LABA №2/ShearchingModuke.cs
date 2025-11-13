de2ed5b3e622fff4bf8c07f354ae2f7672d8cd33
ziqara <tcvegor2006@mail.ru>
2025-11-13 10:13:52 +0300

    Сделал функцию для нахождения индекса команды

--- LR 2/LABA №2/LABA №2/ShearchingModuke.cs
+++ LR 2/LABA №2/LABA №2/ShearchingModuke.cs
@@ -8,5 +8,19 @@ namespace LABA__2
 {
     class ShearchingModuke
     {
+        static public int FindIndexteam(string userQuery, string[] numberofteams) // нахождение индекса команды
+        {
+
+            for (int index = 0; index < numberofteams.Length; ++index)
+            {
+                string team = numberofteams[index].ToLower();
+                if (team == userQuery.ToLower())
+                {
+                    return index;
+                }
+            }
+            return -1;
+
+        }
     }
 }
