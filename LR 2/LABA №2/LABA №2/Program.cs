e7da40f1cbcad870e304393fc28a3f9c576e6508
ziqara <tcvegor2006@mail.ru>
2025-11-13 10:17:54 +0300

    Сделал 2 функции для вывода содержимого

--- LR 2/LABA №2/LABA №2/Program.cs
+++ LR 2/LABA №2/LABA №2/Program.cs
@@ -1,15 +1,23 @@
-﻿using System;
+﻿
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace LABA__2
 {
     class Program
     {
+        static void PrintPlayer(List<string> players)
+        {
+            Console.WriteLine(String.Join(", ", players));
+        }
+        static void PrintGoalsOrScore(List<int> teamspast)
+        {
+            Console.WriteLine(String.Join(", ", teamspast));
+        }
         static void Main()
         {
+
         }
     }
 }
