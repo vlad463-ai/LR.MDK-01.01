60b0f9860e7b3c0ceafc9b0a082a27437861d61b
ziqara <tcvegor2006@mail.ru>
2025-11-13 10:35:47 +0300

    Добавил структуру Phone которая имеет( Model и Price)

---
+++ DistanceEducation/DistanceEducation/Phone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceEducation
+{
+    public struct Phone
+    {
+        public string Model;
+        public int Price;
+    }
+}
