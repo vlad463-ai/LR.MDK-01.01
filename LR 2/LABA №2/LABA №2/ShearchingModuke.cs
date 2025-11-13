3812f1fc125e1e33cdc83f160c246ec91473dcfd
ziqara <tcvegor2006@mail.ru>
2025-11-13 10:14:52 +0300

    Сделал функцию для вывода количество очком команды голов игроков и самих  игроков команды

--- LR 2/LABA №2/LABA №2/ShearchingModuke.cs
+++ LR 2/LABA №2/LABA №2/ShearchingModuke.cs
@@ -22,5 +22,15 @@ namespace LABA__2
             return -1;
 
         }
+        static public (List<int>, List<string>, List<int>) FindingEverythingForTheTeam(int indexteam, List<string>[] players, List<int>[] spast, List<int>[] goalsplauers) // нахождение всего для команды
+        {
+            List<int> teamspast = new List<int>(); //колво очков команды
+            List<string> teamnamess = new List<string>(); // игроки команды
+            List<int> goal = new List<int>(); // колво мячей у человека
+            teamnamess = players[indexteam];
+            teamspast = spast[indexteam];
+            goal = goalsplauers[indexteam];
+            return (teamspast, teamnamess, goal);
+        }
     }
 }
