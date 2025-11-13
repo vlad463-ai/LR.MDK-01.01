050796e12a1e436bf3359d4c814c439ef8edb2f3
ziqara <tcvegor2006@mail.ru>
2025-11-13 10:16:23 +0300

    Реализовал функцию для сортировки игроков

--- LR 2/LABA №2/LABA №2/AnalysisDataModules.cs
+++ LR 2/LABA №2/LABA №2/AnalysisDataModules.cs
@@ -8,5 +8,25 @@ namespace LABA__2
 {
     class AnalysisDataModules
     {
+        static public void SortPlayers(List<string> players, List<int> goals) // сортировка игроков
+        {
+            for (int i = 0; i < goals.Count; i++)
+            {
+                for (int j = 0; j < goals.Count - 1; j++)
+                {
+                    bool condition = goals[j + 1] > goals[j];
+                    if (condition)
+                    {
+                        int temp_goals = goals[j];
+                        goals[j] = goals[j + 1];
+                        goals[j + 1] = temp_goals;
+
+                        string temp_players = players[j];
+                        players[j] = players[j + 1];
+                        players[j + 1] = temp_players;
+                    }
+                }
+            }
+        }
     }
 }
