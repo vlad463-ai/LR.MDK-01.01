9336832f9757da61cf9672bf5ed67afff4e8213a
ziqara <tcvegor2006@mail.ru>
2025-11-13 10:40:29 +0300

    В структуре SalePhone реализовал функцию FindBestAndworstSelling для нахождения лучшего и худшего продаваемого телефона

--- DistanceEducation/DistanceEducation/SalePhone.cs
+++ DistanceEducation/DistanceEducation/SalePhone.cs
@@ -12,6 +12,43 @@ namespace DistanceEducation
         public string PhoneModel;
         public DateTime Date;
 
+        static public void FindBestAndworstSelling(List<SalePhone> sale_phones) // функция находа лучшего и худшего
+        {
+            List<string> Model = new List<string>();
+            List<int> Sold = new List<int>();
 
+            foreach (SalePhone sale in sale_phones)
+            {
+                for (int i = 0; i < sale_phones.Count; i++)
+                {
+                    Model.Add(sale.PhoneModel);
+                    Sold.Add(sale.Sold);
+                }
+            }
+            string BestSellingPhone = " "; // самый продаваемый телефон
+            int phoneSald = 0;
+            for (int i = 0; i < Model.Count; i++)
+            {
+                if (Sold[i] > phoneSald)
+                {
+                    phoneSald = Sold[i];
+                    BestSellingPhone = Model[i];
+                }
+            }
+            string UnsoldPhone = " "; // с наименьшим количеством продаж
+            int PhoneSald = 100;
+            for (int i = 0; i < Model.Count; i++)
+            {
+                if (Sold[i] < PhoneSald)
+                {
+                    PhoneSald = Sold[i];
+                    UnsoldPhone = Model[i];
+                }
+            }
+
+            Console.WriteLine("Самый продаваемый телефон: " + BestSellingPhone);
+            Console.WriteLine("Телефон с маньшим кол-вом продаж: " + UnsoldPhone);
+
+        }
     }
 }
