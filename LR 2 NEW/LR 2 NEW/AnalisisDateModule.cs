8468dba4aad0c0fd819d155b589c73040d6810a1
nikand32543 <vd4216485@gmail.com>
2025-11-21 16:25:04 +0300

    В классе AnalisisDateModule создал функцию СataloguesThisНear, которая выводит список с каталогами этого года

--- LR 2 NEW/LR 2 NEW/AnalisisDateModule.cs
+++ LR 2 NEW/LR 2 NEW/AnalisisDateModule.cs
@@ -1,11 +1,40 @@
 ﻿
 
+using System;
 using System.Collections.Generic;
 
 namespace LR_2_NEW
 {
     class AnalisisDateModule
     {
-        
+        static public List<catalog> СataloguesThisНear(Disk diskС, Disk diskD, string user) // каталоги этого года
+        {
+            List<catalog> Yearthese = new List<catalog>(); // список в котором будут лежать каталоги этого года
+            if (diskС.Name == user)
+            {
+                foreach (catalog catalog in diskС.catalogs)
+                {
+                    if (catalog.yearCreation == 2025)
+                    {
+                        Yearthese.Add(catalog);
+                    }
+                }
+            }
+            else if (diskD.Name == user)
+            {
+                foreach (catalog catalog in diskD.catalogs)
+                {
+                    if (catalog.yearCreation == 2025)
+                    {
+                        Yearthese.Add(catalog);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Указан несуществующий диск выберите один из предложенных: (Диск C, Диск D)");
+            }
+            return Yearthese;
+        }
     }
 }
