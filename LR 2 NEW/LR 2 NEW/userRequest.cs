455c7b47b8426170a305627986b5235e662b6738
nikand32543 <vd4216485@gmail.com>
2025-11-21 16:21:28 +0300

    Создал 2 структуры (Disk и catalog). Заполнил их значениями, а также реализовал функцию InputUserRequest (запрос пользователя)

--- LR 2 NEW/LR 2 NEW/userRequest.cs
+++ LR 2 NEW/LR 2 NEW/userRequest.cs
@@ -5,7 +5,7 @@ namespace LR_2_NEW
 {
     class userRequest
     {
-        static public string InputUserRequest() // Запрос пользователя
+        static public string InputUserRequest() // Запрос пользователя qqq
         {
             Console.WriteLine("Введите пожалуйста букву локального диска (C или D): ");
             return Console.ReadLine();
