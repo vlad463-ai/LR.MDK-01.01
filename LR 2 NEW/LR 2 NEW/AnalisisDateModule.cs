2fc101a726d60e00395e6dd7f6fa43793cd5f49a
nikand32543 <vd4216485@gmail.com>
2025-11-21 16:23:28 +0300

    Создал класс AnalisisDateModule

--- LR 2 NEW/LR 2 NEW/AnalisisDateModule.cs
+++ LR 2 NEW/LR 2 NEW/AnalisisDateModule.cs
@@ -6,16 +6,6 @@ namespace LR_2_NEW
 {
     class AnalisisDateModule
     {
-        static public void СataloguesThisНear(Disk diskС, Disk diskD, string user, List<catalog> C, List<catalog> D) // сортировка каталогов этого года
-        {
-            List<catalog> Yearthese = new List<catalog>();
-            if (diskС.Name == user)
-            {
-                if (diskС.catalogs.Contains(2025))
-                {
-
-                }
-            }
-        }
+        
     }
 }
