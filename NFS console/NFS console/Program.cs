94591a25b2de0b7f398f9111c33510af74e9628c
Y <@gmail.com>
2025-11-22 11:47:09 +0300

    Реализовал функцию для гонки ЛАДА и merc

--- NFS console/NFS console/Program.cs
+++ NFS console/NFS console/Program.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Threading;
 
 
 namespace NFS_console
@@ -18,18 +19,41 @@ namespace NFS_console
             
             Car auto = new Car(); // объект auto
             auto.SetModel("Лада");
-            auto.SetSpeed(1150);
-            Print(auto);
+            auto.SetSpeed(150);
+            //Print(auto);
 
             Car merc = new Car(); // объект merc
             merc.SetModel("Мерс");
             merc.SetSpeed(300);
-            Print(merc);
+            //Print(merc);
 
             List<Car> cars = new List<Car>(); // список для хранения объектов
             cars.Add(auto);
             cars.Add(merc);
 
+
+            LadaVsMerc(cars);
+
+        }
+        static public void LadaVsMerc(List<Car> cars) // функция для гонок
+        {
+            int speed = 1;
+            while (true)
+            {
+                foreach (Car auto in cars)
+                {
+                     for (int i = 0; i <auto.GetSpeed() / 50 * speed; i++)
+                     {
+                        Console.Write(" ");
+                     }
+                    Console.Write(auto.GetModel());
+                    Console.WriteLine();
+                    
+                     speed++;
+                }
+                Thread.Sleep(500);
+                Console.Clear();
+            }
         }
     }
 }
