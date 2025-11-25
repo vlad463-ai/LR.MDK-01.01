728d5cb7f4a5dcc5f0c947b6dc4b338f6af6218a
Y <@gmail.com>
2025-11-25 12:47:13 +0300

    Добавил методы для добавления в класса Storage идентификатора, локации, продукта и его  количетва

--- MenyClassesApplications/MenyClassesApplications/Storage.cs
+++ MenyClassesApplications/MenyClassesApplications/Storage.cs
@@ -6,9 +6,21 @@ namespace MenyClassesApplications
 {
     class Storage //класс (место хранения склад)
     {
-        private int identifier_;
-        private string location_;
-        private Dictionary<Product,int> Products_;
+        private int identifier_; // идентификатор
+        private string location_; // место локации
+        private Dictionary<Product,int> Products_; // словарь с ключом Product(имя и цена) а значение это количество 
         
+        public void  SetIdentifier(int identifier) // Метод добавления идентификатора
+        {
+            identifier_ = identifier;
+        }
+        public void SetLocation(string location) // мотод добавления локации
+        {
+            location_ = location;
+        }
+        public void SetProductQuantity(Product product, int quantity) // метод добавления product(имя и цена) и количества
+        {
+            Products_.Add(product, quantity);
+        }
     }
 }
