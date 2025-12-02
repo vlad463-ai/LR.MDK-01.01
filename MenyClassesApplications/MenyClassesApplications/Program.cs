ec786a5b38444a98e86cee2380404b5d7ff313d1
Y <@gmail.com>
2025-12-02 09:35:26 +0300

    Теперь точно доделал

--- MenyClassesApplications/MenyClassesApplications/Program.cs
+++ MenyClassesApplications/MenyClassesApplications/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace MenyClassesApplications
 {
     class Program
@@ -24,7 +26,33 @@ namespace MenyClassesApplications
             storage.PrintAllMoney(); //Сделал вывод на консоль двух функций для печати
 
 
+            Console.WriteLine();
 
+            Report report = new Report();
+
+            ReportRow row1 = new ReportRow();
+            row1.Product = "ШЫШУЛЯ";
+            row1.Quantity = 500;
+            row1.Price = 100;
+
+            ReportRow row2 = new ReportRow();
+            row2.Product = "ШУШАРИКИ";
+            row2.Quantity = 100;
+            row2.Price = 5192;
+
+            ReportRow row3 = new ReportRow();
+            row3.Product = "Puss in boots";
+            row3.Quantity = 70;
+            row3.Price = 412;
+
+            report.AddRecord(row1);
+            report.AddRecord(row2);
+            report.AddRecord(row3);
+
+            report.ToString();
+            Console.WriteLine();
+
+            report.CalculateSum();
 
 
         }
