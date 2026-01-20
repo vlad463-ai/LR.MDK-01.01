93cb3c349fd8e6b1ca4fa8d32e4441f055d19b43
Y <@gmail.com>
2026-01-20 09:53:15 +0300

    Доделать таймер и еще что то и статистику. Угадай число

---
+++ TextWinForm/TextWinForm/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace TextWinForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelUser = new System.Windows.Forms.Label();
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.CheckBox = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelUser
+            // 
+            this.labelUser.AutoSize = true;
+            this.labelUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelUser.Location = new System.Drawing.Point(7, 67);
+            this.labelUser.Name = "labelUser";
+            this.labelUser.Size = new System.Drawing.Size(323, 29);
+            this.labelUser.TabIndex = 0;
+            this.labelUser.Text = "Введите число от 1 до 100:";
+            // 
+            // InputBox
+            // 
+            this.InputBox.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.InputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.InputBox.Location = new System.Drawing.Point(339, 67);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(100, 35);
+            this.InputBox.TabIndex = 1;
+            // 
+            // CheckBox
+            // 
+            this.CheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CheckBox.Location = new System.Drawing.Point(12, 123);
+            this.CheckBox.Name = "CheckBox";
+            this.CheckBox.Size = new System.Drawing.Size(161, 57);
+            this.CheckBox.TabIndex = 2;
+            this.CheckBox.Text = "Проверить";
+            this.CheckBox.UseVisualStyleBackColor = true;
+            this.CheckBox.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker1.CustomFormat = "MM:ss";
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(122, 35);
+            this.dateTimePicker1.TabIndex = 5;
+            this.dateTimePicker1.Value = new System.DateTime(2026, 1, 20, 0, 1, 0, 0);
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTime.Location = new System.Drawing.Point(154, 17);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(213, 29);
+            this.labelTime.TabIndex = 6;
+            this.labelTime.Text = "- обратный отчёт";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 205);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(424, 96);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.CheckBox);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.labelUser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "Угадай число";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelUser;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.Button CheckBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TextWinForm/TextWinForm/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TextWinForm
+{
+    public partial class MainForm: Form
+    {
+        int number_;
+        public MainForm()
+        {
+            InitializeComponent();
+            Random r = new Random();
+            number_ = r.Next(1, 101);
+            timer1.Start();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(InputBox.Text);
+            if (a > number_)
+            {
+                MessageBox.Show("Ваше число больше загаданного ");
+            }
+            else if (a < number_)
+            {
+                MessageBox.Show("Ваше число меньше загаданного");
+            }
+            else
+            {
+                MessageBox.Show("ПОЗДРАВЛЯЮ!!! Вы угадали число ");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = timer1..
+        }
+    }
+}
