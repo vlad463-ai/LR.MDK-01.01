ddb8be8e203ff76e245f5cbdd683cd44141b957a
Y <@gmail.com>
2026-03-03 09:54:15 +0300

    Изучали WPF проект и надо доделать

---
+++ ChartTest/ChartTest/MainForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ChartTest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.cartesian = new LiveCharts.WinForms.CartesianChart();
+            this.angular = new LiveCharts.WinForms.AngularGauge();
+            this.solid = new LiveCharts.WinForms.SolidGauge();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.cartesian);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "CartesianChart";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.angular);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "AngularChart";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.solid);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(792, 424);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "SolidChart";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // cartesian
+            // 
+            this.cartesian.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cartesian.Location = new System.Drawing.Point(3, 3);
+            this.cartesian.Name = "cartesian";
+            this.cartesian.Size = new System.Drawing.Size(786, 418);
+            this.cartesian.TabIndex = 0;
+            this.cartesian.Text = "cartesian";
+            // 
+            // angular
+            // 
+            this.angular.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.angular.Location = new System.Drawing.Point(3, 3);
+            this.angular.Name = "angular";
+            this.angular.Size = new System.Drawing.Size(786, 418);
+            this.angular.TabIndex = 0;
+            this.angular.Text = "angular";
+            // 
+            // solid
+            // 
+            this.solid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.solid.Location = new System.Drawing.Point(0, 0);
+            this.solid.Name = "solid";
+            this.solid.Size = new System.Drawing.Size(792, 424);
+            this.solid.TabIndex = 0;
+            this.solid.Text = "solid";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "MainForm";
+            this.Text = "LiveCharts-диаграммы";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private LiveCharts.WinForms.CartesianChart cartesian;
+        private LiveCharts.WinForms.AngularGauge angular;
+        private LiveCharts.WinForms.SolidGauge solid;
+    }
+}
+
---
+++ ChartTest/ChartTest/MainForm.cs
@@ -0,0 +1,107 @@
+﻿using LiveCharts;
+using LiveCharts.WinForms;
+using LiveCharts.Wpf;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace ChartTest
+{
+    public partial class MainForm : Form
+    {
+
+        void FillCartesianChart()
+        {
+            LineSeries series = new LineSeries
+            {
+                Title = "Продажи",
+                Values = new ChartValues<int> { 10, 15, 12, 18, 25, 22 },
+
+                Stroke = new SolidColorBrush(Colors.Blue),
+                StrokeThickness = 2,
+
+                PointGeometry = DefaultGeometries.Circle,
+                PointGeometrySize = 7,
+
+                Fill = new LinearGradientBrush(
+                    System.Windows.Media.Color.FromArgb(90, 33, 150, 243),
+                    System.Windows.Media.Color.FromArgb(0, 33, 150, 243),
+                    90)
+            };
+
+            cartesian.Series = new SeriesCollection { series, /*series_2, series_3*/ };
+
+            /// Ось Y
+            cartesian.AxisY.Add(new Axis
+            {
+                Foreground = System.Windows.Media.Brushes.Black,
+                LabelFormatter = value => value.ToString("N0"),
+
+                Separator = new Separator
+                {
+                    Stroke = new SolidColorBrush(Color.FromArgb(40, 0, 0, 0)),
+                    StrokeThickness = 1
+                },
+
+                MaxValue = 30,
+                MinValue = 1
+            }              
+            );
+
+
+            /// Ось X
+            cartesian.AxisX.Add(new Axis
+            {
+                Foreground = System.Windows.Media.Brushes.Black,
+                Labels = new[] { "Янв", "Фев", "Мар", "Апр", "Май", "Июн" },
+
+                Separator = new Separator
+                {
+                    IsEnabled = false,
+                },
+            }
+            );
+        }
+
+        void FillAngular()
+        {
+            angular.Value = 65;
+            angular.FromValue = 0;
+            angular.ToValue = 100;
+
+            angular.TicksForeground = Brushes.Gray;
+            angular.NeedleFill = Brushes.DarkMagenta;
+        }
+
+        void FillSolid()
+        {
+            solid.Value = 40;
+            solid.From = 0;
+            solid.To = 100;
+            solid.LabelFormatter = value => value + "%";
+
+            //   solid.Base.GaugeActiveFill = new System.Windows.Media.LinearGradientBrush();
+            solid.GaugeBackground = Brushes.Red;
+            solid.Base.GaugeActiveFill = new System.Windows.Media.LinearGradientBrush
+            {
+
+                GradientStops = new System.Windows.Media.GradientStopCollection
+                {
+                    new System.Windows.Media.GradientStop(System.Windows.Media.Colors.Yellow, .5),
+                    new System.Windows.Media.GradientStop(System.Windows.Media.Colors.Red, 0),
+                    new System.Windows.Media.GradientStop(System.Windows.Media.Colors.Blue, 1),
+                    
+                }
+            };
+            }
+        public MainForm()
+        {
+            InitializeComponent();
+
+            FillCartesianChart();
+
+            FillAngular();
+
+            FillSolid();
+        }
+    }
+}
