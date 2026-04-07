73c4807944b52ec1ca4cf22647ccceec9d471730
GavrishM <nanotechnology930@gmail.com>
2026-04-07 11:19:16 +0300

    Тренировались завчет

---
+++ Offset/Offset/Program.cs
@@ -0,0 +1,47 @@
+﻿using OffsetLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Offset
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bus b = new bus("Мас-10", 100000, "Торжок");
+            Console.WriteLine(b.GetName());
+
+            List<bus> buses = new List<bus>
+            {
+                new bus("Мас-15",150000,"Торжок"),
+                new bus("Мас-20",200000,"Торжок"),
+                new bus("Мас-25",250000,"Торжок")
+            };
+            Console.WriteLine(buses[2].GetName());
+
+            List<bus> buses1 = allBusesAreSmaller(buses);
+            foreach(var n in buses1)
+            {
+                Console.WriteLine(n.GetName());
+            }
+        }
+        static public List<bus> allBusesAreSmaller(List<bus> buses)
+        {
+            List<bus> busss = new List<bus>();
+            int x = 199000;
+            foreach(var m in buses)
+            {
+                if(x > m.GetPrice())
+                {
+                    busss.Add(m);
+                }
+            }
+            return busss;
+           
+        }
+    }
+}
---
+++ Offset/OffsetLib/bus.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OffsetLib
+{
+    public class bus
+    {
+        private string name_;
+        private double price_;
+        private string manufacturer_;
+
+        public bus( string name,double price, string manufacturer)
+        {
+            name_ = name;
+            price_ = price;
+            manufacturer_ = manufacturer;
+        }
+
+        public string GetName()
+        {
+            return name_;
+        }
+        public double GetPrice()
+        {
+            return price_;
+        }
+
+
+    }
+}
